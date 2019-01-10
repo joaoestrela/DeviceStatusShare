5dd0a364eca722a42d7a0d3783a92dfbe1dc8966
Evilong <evilong_for_life@hotmail.com>
2019-01-10 13:01:03 +0000

    Finish

--- app/src/main/java/pt/up/fc/dcc/devicestatusshare/MainActivity.java
+++ app/src/main/java/pt/up/fc/dcc/devicestatusshare/MainActivity.java
@@ -1,13 +1,275 @@
 package pt.up.fc.dcc.devicestatusshare;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.security.keystore.KeyProperties;
+import android.telephony.TelephonyManager;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.security.NoSuchAlgorithmException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Activity thisActivity = this;
+
+    private static String groupName;
+    private static String groupKey;
+    private static SecretKey secretKey;
+
+    private TextView messages;
+
+    private MyDatagramReceiver myDatagramReceiver = null;
+    private static final int UDP_SERVER_PORT = 2562;
+    private static final int MAX_UDP_DATAGRAM_LEN = 64;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        groupName = intent.getStringExtra("groupName");
+        groupKey = intent.getStringExtra("groupKey");
+        if (groupKey == null || groupKey.isEmpty()) {
+            KeyGenerator keyGen = null;
+            try {
+                keyGen = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            keyGen.init(256);
+            secretKey = keyGen.generateKey();
+            if (secretKey != null) {
+                groupKey = Base64.encodeToString(secretKey.getEncoded(), Base64.DEFAULT);
+            }
+        }
+        else {
+            groupName = intent.getStringExtra("groupName");
+            groupKey = intent.getStringExtra("groupKey");
+            byte[] encodedKey = Base64.decode(groupKey, Base64.DEFAULT);
+            secretKey = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+        }
+        Log.i("MainActivityLog", "GroupName: " + groupName);
+        Log.i("MainActivityLog", "GroupKey: " + groupKey);
         setContentView(R.layout.activity_main);
+        setTitle("Group: " + groupName);
+        messages = findViewById(R.id.messages);
+        Timer timer = new Timer();
+        timer.schedule(new MyDatagramBroadcaster(), 5000, 10000);
+        Button shareButton = findViewById(R.id.buttonShareGroup);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("groupName", groupName);
+                    json.put("groupKey", groupKey);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = barcodeEncoder.encodeBitmap(json.toString(), BarcodeFormat.QR_CODE, 400, 400);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+                //
+                Dialog builder = new Dialog(thisActivity);
+                builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                builder.getWindow().setBackgroundDrawable(
+                        new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        //nothing;
+                    }
+                });
+
+                ImageView imageView = new ImageView(thisActivity);
+                imageView.setImageBitmap(bitmap);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+                builder.show();
+            }
+        });
+    }
+
+    protected void onResume() {
+        super.onResume();
+        myDatagramReceiver = new MyDatagramReceiver();
+        myDatagramReceiver.start();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        myDatagramReceiver.kill();
+    }
+
+    private class MyDatagramBroadcaster extends TimerTask {
+        @Override
+        public void run() {
+            JSONObject sendMessage = new JSONObject();
+            try {
+                sendMessage.put("AndroidID", getDeviceId());
+                sendMessage.put("BatteryStatus", getBatteryPercentage(thisActivity));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            byte [] a = new byte[0];
+            try {
+                a = sendMessage.toString().getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            try {
+                broadcastMessage(encrypt(secretKey.getEncoded(),a));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class MyDatagramReceiver extends Thread {
+        private boolean bKeepRunning = true;
+        private String lastMessage = "";
+
+        public void run() {
+            byte[] lmessage = new byte[MAX_UDP_DATAGRAM_LEN];
+            DatagramPacket packet = new DatagramPacket(lmessage, lmessage.length);
+            try {
+                DatagramSocket socket = new DatagramSocket(UDP_SERVER_PORT,InetAddress.getByName("0.0.0.0"));
+                socket.setBroadcast(true);
+                while (bKeepRunning) {
+                    socket.receive(packet);
+                    lastMessage = new String(decrypt(secretKey.getEncoded(),lmessage),"UTF-8");
+                    runOnUiThread(updateTextMessages);
+                }
+                socket.close();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+        String getLastMessage() {
+            return lastMessage;
+        }
+
+        void kill() {
+            bKeepRunning = false;
+        }
+    }
+
+    private Runnable updateTextMessages = new Runnable() {
+        public void run() {
+            if (myDatagramReceiver == null) return;
+            if (messages.getText().length() == 0)
+                messages.setText(myDatagramReceiver.getLastMessage());
+            else
+                messages.setText(messages.getText() + "\n" + myDatagramReceiver.getLastMessage());
+        }
+    };
+
+    private void broadcastMessage(byte[] sendData) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            socket.setBroadcast(true);
+            InetAddress IPAddress = getBroadcastAddress();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, UDP_SERVER_PORT);
+            Log.i("BroadcastLog", IPAddress.getHostAddress());
+            socket.send(sendPacket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getBatteryPercentage(Context context) {
+        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, iFilter);
+        int level = batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) : -1;
+        int scale = batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1) : -1;
+        float batteryPct = level / (float) scale;
+        return (int) (batteryPct * 100);
+    }
+
+    private String getDeviceId() {
+        String deviceId = "";
+        final TelephonyManager mTelephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(thisActivity, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+        }
+        if (mTelephony.getDeviceId() != null) {
+            deviceId = mTelephony.getDeviceId();
+        } else {
+            deviceId = Settings.Secure.getString(getApplicationContext()
+                    .getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+        return deviceId;
+    }
+
+    private static byte[] encrypt(byte[] raw, byte[] clear) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        return cipher.doFinal(clear);
+    }
+
+    private static byte[] decrypt(byte[] raw, byte[] encrypted) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        return cipher.doFinal(encrypted);
+    }
+
+    InetAddress getBroadcastAddress() throws IOException {
+        WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcp = wifi.getDhcpInfo();
+        int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+        byte[] quads = new byte[4];
+        for (int k = 0; k < 4; k++)
+            quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+        return InetAddress.getByAddress(quads);
     }
 }
--- app/src/main/java/pt/up/fc/dcc/devicestatusshare/StartActivity.java
+++ app/src/main/java/pt/up/fc/dcc/devicestatusshare/StartActivity.java
@@ -1,13 +1,91 @@
 package pt.up.fc.dcc.devicestatusshare;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Activity thisActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button joinGroup = findViewById(R.id.join_group_btn);
+        Button createGroup = findViewById(R.id.create_group_btn);
+        joinGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new IntentIntegrator(thisActivity).initiateScan();
+            }
+        });
+        createGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(thisActivity);
+                builder.setTitle("Group Name:");
+                final EditText input = new EditText(thisActivity);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent myIntent = new Intent(StartActivity.this, MainActivity.class);
+                        myIntent.putExtra("groupName", input.getText().toString());
+                        StartActivity.this.startActivity(myIntent);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        JSONObject jsonObject = null;
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                try {
+                    jsonObject = new JSONObject(result.getContents());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Intent myIntent = new Intent(StartActivity.this, MainActivity.class);
+                try {
+                    if(jsonObject != null && jsonObject.getString("groupName")!= null) myIntent.putExtra("groupName", jsonObject.getString("groupName"));
+                    if(jsonObject != null && jsonObject.getString("groupKey")!= null) myIntent.putExtra("groupName", jsonObject.getString("groupName"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                StartActivity.this.startActivity(myIntent);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
